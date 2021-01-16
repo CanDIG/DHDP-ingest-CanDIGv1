d02c74da192d7470b7b7aa8dbf857906f81bff87
Jonathan Dursi <jonathan@dursi.ca>
2021-01-16 10:07:16 -0500

    Updated based on feedback

--- COMPARISON/data_ingest.py
+++ COMPARISON/data_ingest.py
@@ -13,7 +13,48 @@ from datetime import datetime
 from collections import defaultdict
 from dateutil import relativedelta
 
-#
+def date_from_datetime(datetime_str):
+    """
+    Give an string YY/MM/DD HH:MM:SS, return just the date string.
+    """
+    # doing this in the simplest possible way now - just returning
+    # the first part of the string split at whitespace.  No validation
+    # done.
+    words = datetime_str.split()
+    if not words:
+        return ""
+    return words[0]
+
+def province_from_site(site_str):
+    """
+    Infers the province from the site string
+    """
+    site_mapping = {"BCCA Vancouver Cancer Centre": "British Columbia",
+                    "Princess Margaret Cancer Centre": "Ontario"}
+
+    result = "Unknown"
+    site = site_str.strip()
+    if site in site_mapping:
+        result = site_mapping[site]
+
+    return result
+
+def outcome_label(patient_id, increment=False):
+    """
+    Generates current outcome label, using the global
+    table_to_counts
+    """
+    global table_to_counts
+
+    # inside the reading loop, incrementing is handled
+    # automatically; but to add final vital status, we
+    # need to explicilty increment
+    if increment:
+        table_to_counts["Outcome"][patient_id] += 1
+
+    count = table_to_counts["Outcome"][patient_id]
+    return f"{patient_id}_outcome_{count}"
+
 # Below are the mappings from the CSV files to the elements of the CanDIGv1 data model
 # The dictionary has keys for each of the relevant csv files (the DataPageName from
 # medidata rave).  Then item is a list of tuples of CanDIGv1 table name, and then mapping
@@ -27,14 +68,30 @@ from dateutil import relativedelta
 #
 # means that in the medidata rave files, demographics.GENDER_CODE -> Patient.gender
 #
+# Also, rather than a field name like "Subject", the item to map can be a tuple,
+# consisting of a function and a number of fields it takes as parameters:  e.g.
+#
+#        ("Diagnosis", {
+#           "patientId": "Subject",
+#           "cancerType": (lambda s: s.lower(), "MALIGNANCY"),
+#        })
+#
+# means that the results in the field MALIGNINANCY are lowercased and then placed into
+# Diagnosis.cancerType.  Functions with zero arguments can be useful for setting constants:
+#
+#             "vitalStatus": (lambda : "Dead",),
+#
+# will always set the vitalStatus field to "Dead" for patients in that table
+
 
 section_to_mapping_types = {
     "demographics": [
         ("Patient", {
             "patientId": "Subject",
             "gender": "GENDER_CODE",
-            "dateOfBirth": "BIRTH_DATE_YM_INT",
-            "ethnicity": "ETHNICITY_CODE_STD"
+            "dateOfBirth": (date_from_datetime, "BIRTH_DATE_YM_INT"),
+            "ethnicity": "ETHNICITY_CODE",
+            "provinceOfResidence": (province_from_site, "Site")
         }),
         ("Enrollment", {
             "patientId": "Subject",
@@ -44,7 +101,7 @@ section_to_mapping_types = {
     "diagnosis": [
         ("Diagnosis", {
             "patientId": "Subject",
-            "diagnosisDate": "DIAG_PA",
+            "diagnosisDate": (date_from_datetime, "DIAG_PA"),
             "cancerType": (lambda s: s.lower(), "MALIGNANCY"),
             "histology": (lambda s: s.lower(), "HISTO_CYTO_DIAG"),
             "tumorGrade": "GRADE_DIAG",
@@ -56,9 +113,17 @@ section_to_mapping_types = {
         ("Treatment", {
             "patientId": "Subject",
             "therapeuticModality": (lambda s: s.lower(), "THER_TX_NAME"),
-            "startDate": "STRT_DT",
-            "stopDate": "LAST_DATE",
+            "startDate": (date_from_datetime, "STRT_DT"),
+            "stopDate": (date_from_datetime, "LAST_DATE"),
             "responseToTreatment": "THER_BR"
+        })
+    ],
+
+    "follow-up patient status": [
+        ("Outcome", {
+            "dateOfAssessment": (date_from_datetime, "FU_STATUS_DT"),
+            "diseaeResponseOrStatus": (lambda s: s.strip('"'), "DISEASE_STATUS"),
+            "localId": (outcome_label, "Subject")
         })
     ],
 
@@ -66,7 +131,7 @@ section_to_mapping_types = {
         ("Sample", {
             "patientId": "Subject",
             "sampleId": "SAMPLE_ID",
-            "collectionDate": "SURG_DT",
+            "collectionDate": (date_from_datetime, "SURG_DT"),
             "sampleType": "ARCH_TMR_SITE",
             "cancerType": (lambda s: s.lower(), "CANCER_TYPE_LONG")
         })
@@ -75,19 +140,21 @@ section_to_mapping_types = {
     "death": [
         ("Patient", {
             "patientId": "Subject",
-            "dateOfDeath": "DTH_DT"
+            "dateOfDeath": (date_from_datetime, "DTH_DT")
         }),
         ("Outcome", {
             "patientId": "Subject",
             "vitalStatus": (lambda : "Dead",),
-            "dateOfAssessment": "DTH_DT"
+            "dateOfAssessment": "DTH_DT",
+            "localId": (outcome_label, "Subject")
         })
     ]
 }
 
 table_to_counts = {"Treatment": defaultdict(int),
                    "Diagnosis": defaultdict(int),
-                   "Enrollment": defaultdict(int)}
+                   "Enrollment": defaultdict(int),
+                   "Outcome": defaultdict(int)}
 patient_to_data = {}
 
 
@@ -129,8 +196,8 @@ def update_patient_data(row):
             else:  # if the value of the key is of a tuple, 
                 # function and argument namees
                 t = mapping_dict[key]
-                function = mapping_dict[key][0]
-                args = (row[item] for item in t[1:] if item in row)
+                function, argitems = t[0], t[1:]
+                args = (row[item] for item in argitems if item in row)
                 new_dict[key] = function(*args)
 
         if counter:
@@ -172,21 +239,37 @@ def main():
             for row in reader:
                 update_patient_data(row)
 
-    for patient in patient_to_data:
-        if not "Outcome" in patient_to_data[patient]:
-            patient_to_data[patient]["Outcome"] = {
-                "patientId": patient,
-                "localId": f"{patient}_outcome_1",
-                "vitalStatus": "Alive"
-            }
+    # update vital status and keys that depend on same
+    for patient_id in patient_to_data:
+        new_local_id = outcome_label(patient_id, increment=True)
+        if not "dateOfDeath" in patient_to_data[patient_id]["Patient"]:
+            if not "Outcome" in patient_to_data[patient_id]:
+                patient_to_data[patient_id]["Outcome"] = {"localId": new_local_id}
+
+            outcomes = patient_to_data[patient_id]["Outcome"]
+            if isinstance(outcomes, dict):
+                patient_to_data[patient_id]["Outcome"]["vitalStatus"] = "Alive"
+            elif isinstance(outcomes, list):
+                patient_to_data[patient_id]["Outcome"][-1]["vitalStatus"] = "Alive"
         else:
-            if "Diagnosis" in patient_to_data[patient] and\
-                    len(patient_to_data[patient]["Diagnosis"]["diagnosisDate"]) > 0:
-                diagnosis_date = datetime.strptime(patient_to_data[patient]["Diagnosis"]["diagnosisDate"].split()[0], "%m/%d/%Y")
-                death_date = datetime.strptime(patient_to_data[patient]["Patient"]["dateOfDeath"].split()[0], "%m/%d/%Y")
+            new_dict = {"vitalStatus": "Dead", "localId": new_local_id}
+            if "Diagnosis" in patient_to_data[patient_id] and\
+                    len(patient_to_data[patient_id]["Diagnosis"]["diagnosisDate"]) > 0:
+                diagnosis_date = datetime.strptime(patient_to_data[patient_id]["Diagnosis"]["diagnosisDate"], "%m/%d/%Y")
+                death_date = datetime.strptime(patient_to_data[patient_id]["Patient"]["dateOfDeath"], "%m/%d/%Y")
+                new_dict["dateOfAssessment"] = str(death_date)
                 dates_diff = relativedelta.relativedelta(death_date, diagnosis_date)
                 survival_in_months = (dates_diff.years * 12) + dates_diff.months + (dates_diff.days / 30)
-                patient_to_data[patient]["Outcome"]["overallSurvivalInMonths"] = str(survival_in_months)
+                new_dict["overallSurvivalInMonths"] = str(survival_in_months)
+
+            if not "Outcome" in patient_to_data[patient_id]:
+                patient_to_data[patient_id]["Outcome"] = new_dict
+            elif isinstance(patient_to_data[patient_id]["Outcome"], dict):
+                patient_to_data[patient_id]["Outcome"] = [patient_to_data[patient_id]["Outcome"], new_dict]
+            elif isinstance(patient_to_data[patient_id]["Outcome"], list):
+                patient_to_data[patient_id]["Outcome"].append(new_dict)
+
+# patient_to_data[patient_id]["Outcome"][local_id]["overallSurvivalInMonths"] = str(survival_in_months)
 
     output_dict = {"metadata": list(patient_to_data.values())}
     json.dump(output_dict, outfile)
